9c256a145ac45c39e7e25533adf4120858bde65d
kumoren <mnievas640@gmail.com>
2026-08-18 10:39:39 -0300

    Added system level packages

---
+++ modules/core/modules/systemPackages.nix
@@ -0,0 +1,41 @@
+{ pkgs, pkgs-stable, config, ... }:
+{
+  environment.systemPackages =
+  [
+    # basic
+    pkgs.tree
+    pkgs.helix
+    pkgs.neovim
+    pkgs.vim
+    pkgs.kitty
+    pkgs.wget
+    pkgs.yazi
+    pkgs.nh
+    pkgs.nix-output-monitor
+    pkgs.nvd
+
+    # Utility
+    pkgs.power-profiles-daemon
+    pkgs.starship
+    pkgs.less
+    pkgs.git
+    pkgs.udiskie
+    pkgs.killall
+    pkgs.ripgrep
+    pkgs.xdg-desktop-portal
+    pkgs.xdg-desktop-portal-gnome
+    pkgs.xdg-desktop-portal-gtk
+    pkgs.xdg-desktop-portal-wlr
+    pkgs.eza
+
+    # micelanious
+    pkgs.xfce.thunar
+    pkgs.bibata-cursors
+    pkgs.greetd
+    pkgs.tuigreet
+    pkgs.less
+    pkgs.pavucontrol
+    pkgs.inotify-tools
+    pkgs.wl-clipboard
+  ];
+}
