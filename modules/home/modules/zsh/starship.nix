7561210f197ae5173a102f1462bd12d6e6fc1afd
kumoren <mnievas640@gmail.com>
2026-08-18 17:14:03 -0300

    configured starship

---
+++ modules/home/modules/zsh/starship.nix
@@ -0,0 +1,70 @@
+{ pkgs, lib, ... }:
+{
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = false;
+    settings = {
+      add_newline = true;
+
+      format = lib.concatStrings [
+        "  $hostname/"
+        "$username"
+        "$directory"
+        "$fill"
+        "$git_branch"
+        "$git_status"
+        "$nix_shell"
+        "$line_break"
+        "  $character"
+      ];
+
+      fill = {
+        symbol = " ";
+      };
+
+      nix_shell = {
+        symbol = "";
+        disabled = false;
+        style = "bold blue";
+        format = " via [$symbol$state(\($name\))]($style) ";
+      };
+
+      character = {
+        success_symbol = "[>>](bold blue)";
+        error_symbol = "[❮](bold red)";
+        vicmd_symbol = "[❯](bold cyan)";
+      };
+
+      username = {
+        disabled = false;
+        format = "[$user]($style)";
+        style_user = "bold blue";
+        show_always = true;
+      };
+
+      hostname = {
+        disabled = false;
+        format = "[$hostname]($style)";
+        style = "bold red";
+        ssh_only = false;
+      };
+
+      directory = {
+        disabled = false;
+        format = " -> [$path]($style)";
+        style = "bold cyan";
+        truncation_length = 2;
+      };
+
+      time = {
+        disabled = false;
+        style = "bold red";
+        format = "󰔟[$time]($style) ";
+        time_format = "%H%p-%M";
+      };
+
+    };
+  };
+
+}
